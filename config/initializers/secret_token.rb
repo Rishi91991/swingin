fe3ee059513e12df61429b77f68beaa0ab7c54b1
Rishi Mehta <rishi91991@gmail.com>
2013-11-25 17:16:30 +0530

    Gem file updated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swingin::Application.config.secret_token = 'e35a268749d24e872dc0bfd7d1806e8f4a08ecdb0aaa40baf45182aa1af110fb8ef932ae06eda5727f34e5bc9138935e64827785cb2c1a62db874fe2d6c5389d'
